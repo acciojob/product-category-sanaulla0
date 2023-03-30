038960da759c7f7627b7a3975954fd5e357f4ae7
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-30 14:53:17 +0530

    p2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [gender,setGender] = useState(' ');
+  const male  = {
+     a: 'Shirts', b: 'Facial grooming kits', c: "Hair styling",d:"Jeans", e: "Shoes"}
+ 
+  const female = 
+    {  a:'Tops', b: 'Skirts' , c: 'Makeup items', d: 'Jewellery', e:'Sandals'}
+   const handleClick = (e)=>{
+            setGender(e.target.value) 
+   }
+
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+    <select value={gender} onChange={handleClick} >
+      <option value='male' default={male}>Male</option>
+      <option value='female'>Female</option>
+    </select>
+    <div>
+    <ul>
+     {gender==="male" && Object.keys(male).map((item)=>(
+     
+                   <li key={item}>{male[item]}</li>
+                   
+     ))}
+     {gender==="female" && Object.keys(female).map((key)=>(
+              <li key={key}>{female[key]}</li>
+     ))}
+
+    </ul>
     </div>
-  )
-}
+                  <hr/>
+    </div>
+  );
+};
 
 export default App
